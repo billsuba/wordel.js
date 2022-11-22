0a23b00c3f3970224ca96e554a342b4bd8bc234d
billsuba <84655267+billsuba@users.noreply.github.com>
2022-11-22 05:48:12 -0500

    Update index.js
    decreased line of code
    
    improved while loop conditions
    
    added an array parameter for the function wordel

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let userName = prompt(`Welcome to Wordel! What's your name?`);
+let again = false;
 
-function wordel(again) {
-  // array of possible words to guess
-  let objOfWords = ['about', 'above', 'after', 'again', 'along', 'among', 'asked', 'based', 'began', 'being', 'black', 'cases', 'cause', 'child', 'class', 'clear', 'could', 'court', 'death', 'early', 'every', 'field', 'first', 'force', 'found', 'given', 'going', 'great', 'group', 'heart', 'house', 'human', 'known', 'large', 'later', 'least', 'level', 'light', 'local', 'major', 'means', 'might', 'model', 'money', 'never', 'night', 'north', 'often', 'order', 'other', 'party', 'place', 'point', 'power', 'press', 'quite', 'right', 'seems', 'sense', 'shall', 'short', 'shown', 'since', 'small', 'south', 'state', 'still', 'study', 'table', 'taken', 'terms', 'their', 'there', 'these', 'thing', 'think', 'third', 'those', 'three', 'times', 'total', 'trade', 'under', 'until', 'using', 'value', 'water', 'where', 'which', 'while', 'white', 'whole', 'whose', 'woman', 'women', 'words', 'world', 'would', 'years', 'young'];
+// array of possible words to guess
+let arr1 = ['about', 'above', 'after', 'again', 'along', 'among', 'asked', 'based', 'began', 'being', 'black', 'cases', 'cause', 'child', 'class', 'clear', 'could', 'court', 'death', 'early', 'every', 'field', 'first', 'force', 'found', 'given', 'going', 'great', 'group', 'heart', 'house', 'human', 'known', 'large', 'later', 'least', 'level', 'light', 'local', 'major', 'means', 'might', 'model', 'money', 'never', 'night', 'north', 'often', 'order', 'other', 'party', 'place', 'point', 'power', 'press', 'quite', 'right', 'seems', 'sense', 'shall', 'short', 'shown', 'since', 'small', 'south', 'state', 'still', 'study', 'table', 'taken', 'terms', 'their', 'there', 'these', 'thing', 'think', 'third', 'those', 'three', 'times', 'total', 'trade', 'under', 'until', 'using', 'value', 'water', 'where', 'which', 'while', 'white', 'whole', 'whose', 'woman', 'women', 'words', 'world', 'would', 'years', 'young'];
 
-  if (again === true || confirm(`Would you like to play Wordel ${userName}?`)) {
-    let wordOfTheDay = objOfWords[Math.floor(Math.random() * objOfWords.length)];
+function wordel(again, arrOfWords) {
+  if (again || confirm(`Would you like to play Wordel ${userName}?`)) {
+    let wordOfTheDay = arrOfWords[Math.floor(Math.random() * arrOfWords.length)];
 
     // gameplay('happy'); // input a specific word for testing purposes
-
     gameplay(wordOfTheDay);
     return;
   }
@@ -16,35 +16,26 @@ function wordel(again) {
   return;
 
   function gameplay(word) {
-    // console.log(word); // uncomment for hacks
-
+    console.log(word); // uncomment for hacks
     word = word.split('');
 
     // makes sure it passes only if its a number that is greater than 0
     let lives = prompt('How many lives do you want?');
-    if (lives <= 0) lives = 'redo';
 
-    while (isNaN(lives)) {
-      lives = prompt('Please enter a valid number');
-      if (lives <= 0) lives = 'redo';
-    }
+    while (isNaN(lives) || lives <= 0) lives = prompt('Please enter a valid number');
 
     let final = ['*', '*', '*', '*', '*'];
-
     let winOrLose = 'Play Again?';
 
     while (lives > 0) {
       // makes sure it passes only if its five letters
       let guess = prompt('Guess a five letter word');
-      if (/^[a-zA-Z]+$/.test(guess) === false) guess = 0;
 
-      while (guess.length !== 5) {
-        guess = prompt('Please enter five letters');
-        if (/^[a-zA-Z]+$/.test(guess) === false) guess = 0;
-      }
-      guess = guess.toLowerCase().split('');
+      while (guess.length !== 5 || (/^[a-zA-Z]+$/.test(guess) === false)) guess = prompt('Please enter five letters');
 
       let wrongPosition = [];
+      guess = guess.toLowerCase().split('');
+      lives--;
 
       guess.forEach((el, i) => {
         if (el === word[i]) {
@@ -58,14 +49,11 @@ function wordel(again) {
       })
 
       let after = ` but ${wrongPosition} was in the wrong postion`;
-      if (wrongPosition.length <= 0) {
+      if (wrongPosition.length === 0) {
         alert(`You guessed ${final} right`);
       } else {
         alert(`You guessed ${final} right${after}`);
-        after = 0;
       }
-
-      lives--;
 
       if (guess.join('') === word.join('')) {
         alert('Congrats!');
@@ -77,9 +65,9 @@ function wordel(again) {
         alert(`You have ${lives} lives left`);
       }
     };
-    let again = true;
-    confirm(`${winOrLose}`) ? wordel(again) : alert('See you soon!');
+    again = true;
+    confirm(`${winOrLose}`) ? wordel(again, arrOfWords) : alert('See you soon!');
   }
 }
 
-wordel();
+wordel(again, arr1);

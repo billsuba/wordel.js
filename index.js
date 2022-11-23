f2acb20e34a485f5ac5b450e194b368d01859f2f
billsuba <84655267+billsuba@users.noreply.github.com>
2022-11-23 02:03:39 -0500

    Update index.js
    reduced line of code
    
    strictly game only by removing name prompt

--- index.js
+++ index.js
@@ -1,63 +1,52 @@
-let userName = prompt(`Welcome to Wordel! What's your name?`);
-let again = false;
-
 // array of possible words to guess
 let arr1 = ['about', 'above', 'after', 'again', 'along', 'among', 'asked', 'based', 'began', 'being', 'black', 'cases', 'cause', 'child', 'class', 'clear', 'could', 'court', 'death', 'early', 'every', 'field', 'first', 'force', 'found', 'given', 'going', 'great', 'group', 'heart', 'house', 'human', 'known', 'large', 'later', 'least', 'level', 'light', 'local', 'major', 'means', 'might', 'model', 'money', 'never', 'night', 'north', 'often', 'order', 'other', 'party', 'place', 'point', 'power', 'press', 'quite', 'right', 'seems', 'sense', 'shall', 'short', 'shown', 'since', 'small', 'south', 'state', 'still', 'study', 'table', 'taken', 'terms', 'their', 'there', 'these', 'thing', 'think', 'third', 'those', 'three', 'times', 'total', 'trade', 'under', 'until', 'using', 'value', 'water', 'where', 'which', 'while', 'white', 'whole', 'whose', 'woman', 'women', 'words', 'world', 'would', 'years', 'young'];
 
-function wordel(again, arrOfWords) {
-  if (again || confirm(`Would you like to play Wordel ${userName}?`)) {
-    let wordOfTheDay = arrOfWords[Math.floor(Math.random() * arrOfWords.length)];
+function gameplay(arrOfWords) {
+  let word = arrOfWords[Math.floor(Math.random() * arrOfWords.length)];
 
-    // gameplay('happy'); // input a specific word for testing purposes
-    return gameplay(wordOfTheDay);
-  };
-  return alert('Your loss');
+  // console.log(word); // uncomment for hacks
+  word = word.split('');
+  let winOrLose = 'Play Again?';
 
-  function gameplay(word) {
-    // console.log(word); // uncomment for hacks
-    word = word.split('');
-    let winOrLose = 'Play Again?';
+  // makes sure it passes only if its a number that is greater than 0
+  let lives = prompt('How many lives do you want?');
+  while (isNaN(lives) || lives <= 0) lives = prompt('Please enter a valid number');
 
-    // makes sure it passes only if its a number that is greater than 0
-    let lives = prompt('How many lives do you want?');
-    while (isNaN(lives) || lives <= 0) lives = prompt('Please enter a valid number');
+  while (lives > 0) {
+    // makes sure it passes only if its five letters
+    let guess = prompt('Guess a five letter word');
+    while (guess.length !== 5 || (/^[a-zA-Z]+$/.test(guess) === false)) guess = prompt('Please enter five letters');
 
-    while (lives > 0) {
-      // makes sure it passes only if its five letters
-      let guess = prompt('Guess a five letter word');
-      while (guess.length !== 5 || (/^[a-zA-Z]+$/.test(guess) === false)) guess = prompt('Please enter five letters');
+    let final = ['*', '*', '*', '*', '*'];
+    let wrongPosition = [];
+    guess = guess.toLowerCase().split('');
+    lives--;
 
-      let final = ['*', '*', '*', '*', '*'];
-      let wrongPosition = [];
-      guess = guess.toLowerCase().split('');
-      lives--;
-
-      // checks if letters are in word
-      guess.forEach((el, i) => {
-        if (el === word[i]) {
-          final[i] = el;
-        } else if (word.includes(el)) {
-          wrongPosition.push(el);
-        };
-      });
-
-      let before = `You guessed ${final} right`;
-      let after = ` but ${wrongPosition} was in the wrong postion`;
-      wrongPosition.length === 0 ? alert(before) : alert(`${before}${after}`);
-
-      if (guess.join('') === word.join('')) {
-        alert('Congrats!');
-        lives = 0;
-      } else if (lives === 0) {
-        alert(`The word was ${word.join('')}`);
-        winOrLose = 'Try Again?';
-      } else {
-        alert(`You have ${lives} lives left`);
+    // checks if letters are in word
+    guess.forEach((el, i) => {
+      if (el === word[i]) {
+        final[i] = el;
+      } else if (word.includes(el)) {
+        wrongPosition.push(el);
       };
+    });
+
+    let before = `You guessed ${final} right`;
+    let after = ` but ${wrongPosition} was in the wrong postion`;
+    wrongPosition.length === 0 ? alert(before) : alert(`${before}${after}`);
+
+    if (guess.join('') === word.join('')) {
+      alert('Congrats!');
+      lives = 0;
+    } else if (lives === 0) {
+      alert(`The word was ${word.join('')}`);
+      winOrLose = 'Try Again?';
+    } else {
+      alert(`You have ${lives} lives left`);
     };
-    again = true;
-    confirm(`${winOrLose}`) ? wordel(again, arrOfWords) : alert('See you soon!');
   };
+  again = true;
+  confirm(`${winOrLose}`) ? gameplay(arr1) : alert('See you soon!');
 };
 
-wordel(again, arr1);
+gameplay(arr1);
